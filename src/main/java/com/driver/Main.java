6c440bde8aecab0bf34c923212824371d63b3af3
unknown <hemant38056@gmail.com>
2022-12-16 00:18:39 +0530

    Submission1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly rwObject = new RWOnly();
+        rwObject.set("Hello");
+        System.out.println(rwObject.get());
+    }
+
 }
